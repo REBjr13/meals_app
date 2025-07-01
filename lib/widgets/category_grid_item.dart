cc3083b9b5466a7752595af87577ca10cb4b91b4
Robert Bob-Manuel <rebcave8@gmail.com>
2025-07-01 22:23:05 +0100

    added folders and new files

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
